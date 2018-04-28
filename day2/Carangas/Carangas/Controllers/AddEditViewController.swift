6634f89bb383f10de04c63b4b592e363a4a5e6ab
Douglas Frari <douglas_delfrari@apple.com>
2018-04-28 13:15:51 -0300

    Projeto da aula - parte 2

---
+++ day2/Carangas/Carangas/Controllers/AddEditViewController.swift
@@ -0,0 +1,209 @@
+//
+//  AddEditViewController.swift
+//  Carangas
+//
+//  Created by Eric Brito.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+enum CarOperationAction {
+    case add_car
+    case edit_car
+    case get_brands
+}
+
+class AddEditViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tfBrand: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPrice: UITextField!
+    @IBOutlet weak var scGasType: UISegmentedControl!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+
+    var brands: [Brand]!
+    
+    // MARK: - Properties
+    var car: Car!
+    
+    lazy var pickerView: UIPickerView = {
+        let picker = UIPickerView()
+        picker.backgroundColor = .white
+        picker.delegate = self
+        picker.dataSource = self
+        
+        return picker
+    } ()
+    
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1 - modo edicao
+        if car != nil {
+            
+            tfBrand.text = car.brand
+            tfName.text = car.name
+            tfPrice.text = "\(car.price)"
+            scGasType.selectedSegmentIndex = car.gasType
+            btAddEdit.setTitle("Alterar carro", for: .normal)
+        }
+        
+        
+        // 1
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        toolbar.tintColor = UIColor(named: "main")
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let btSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [btCancel, btSpace, btDone]
+        
+        tfBrand.inputAccessoryView = toolbar
+        tfBrand.inputView = pickerView
+        
+        loadBrands()
+    }
+    
+    
+    
+    // MARK: - IBActions
+    @IBAction func addEdit(_ sender: UIButton) {
+        
+        if car == nil {
+            // adicionar carro novo
+            car = Car()
+        }
+        
+        car.name = (tfName?.text)!
+        car.brand = (tfBrand?.text)!
+        if tfPrice.text!.isEmpty {
+            tfPrice.text = "0"
+        }
+        car.price = Double(tfPrice.text!)!
+        car.gasType = scGasType.selectedSegmentIndex
+        
+        // 1 - diferenciar se estamos salvando (SAVE) ou editando (UPDATE_
+        if car._id == nil {
+            // new car
+            REST.save(car: car) { (success) in
+                self.goBack()
+            }
+        } else {
+            // 2 - edit current car
+            REST.update(car: car) { (success) in
+                self.goBack()
+            }
+        }
+        
+        
+    }
+    
+    func goBack() {
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @objc func cancel() {
+        tfBrand.resignFirstResponder()
+    }
+    
+    @objc func done() {
+        tfBrand.text = brands[pickerView.selectedRow(inComponent: 0)].fipe_name
+        cancel()
+    }
+    
+    func loadBrands() {
+        
+        REST.loadBrands { (brands) in
+            guard let brands = brands else {return}
+            
+            // ascending order
+            self.brands = brands.sorted(by: {$0.fipe_name < $1.fipe_name})
+            
+            DispatchQueue.main.async {
+                self.pickerView.reloadAllComponents()
+            }
+            
+        }
+    }
+    
+    func startLoadingAnimation() {
+        self.btAddEdit.isEnabled = false
+        self.btAddEdit.backgroundColor = .gray
+        self.btAddEdit.alpha = 0.5
+        self.loading.startAnimating()
+    }
+    
+    func stopLoadingAnimation() {
+        self.btAddEdit.isEnabled = true
+        self.btAddEdit.backgroundColor = UIColor(named: "main")
+        self.btAddEdit.alpha = 0
+        self.loading.stopAnimating()
+    }
+    
+    func showAlert(withTitle titleMessage: String, withMessage message: String, isTryAgain hasRetry: Bool, operation oper: CarOperationAction) {
+        
+        if oper != .get_brands {
+            DispatchQueue.main.async {
+                // ?
+            }
+            
+        }
+        
+        let alert = UIAlertController(title: titleMessage, message: message, preferredStyle: .actionSheet)
+        
+        if hasRetry {
+            let tryAgainAction = UIAlertAction(title: "Tentar novamente", style: .default, handler: {(action: UIAlertAction) in
+                
+                switch oper {
+                case .add_car: break
+                // ?
+                case .edit_car: break
+                // ?
+                case .get_brands: break
+                    // ?
+                }
+                
+            })
+            alert.addAction(tryAgainAction)
+            
+            let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: {(action: UIAlertAction) in
+                self.goBack()
+            })
+            alert.addAction(cancelAction)
+        }
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+}
+
+
+extension AddEditViewController:UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // MARK: - UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let brand = brands[row]
+        return brand.fipe_name
+    }
+    
+    
+    // MARK: - UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return brands.count
+    }
+}
+
