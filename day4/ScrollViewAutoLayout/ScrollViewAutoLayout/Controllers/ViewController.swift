7abf736fad71273a57f71f8b8cde85da93d9271f
Douglas Frari <douglas.frari@gmail.com>
2018-05-14 20:53:32 -0300

    UIScrollView demonstration project

---
+++ day4/ScrollViewAutoLayout/ScrollViewAutoLayout/Controllers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScrollViewAutoLayout
+//
+//  Created by Douglas Frari on 14/05/2018.
+//  Copyright © 2018 Douglas Frari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
