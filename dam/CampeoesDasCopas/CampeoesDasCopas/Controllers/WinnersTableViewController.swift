6aee349d35e6d8ac5c2f2555def08d1c2ac72d37
Douglas Frari <douglas.frari@gmail.com>
2018-05-25 08:21:59 -0300

    Mudanca de pastas (iot e dam)

---
+++ dam/CampeoesDasCopas/CampeoesDasCopas/Controllers/WinnersTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  WinnersTableViewController.swift
+//  CampeoesDasCopas
+//
+//  Created by Douglas Frari on 24/05/18.
+//  Copyright © 2018 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class WinnersTableViewController: UITableViewController {
+
+    var worldCups: [WorldCup] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadWorldCups()
+    }
+    
+    func loadWorldCups() {
+        let fileURL = Bundle.main.url(forResource: "winners", withExtension: ".json")!
+        let jsonData = try! Data(contentsOf: fileURL)
+        
+        do {
+            worldCups = try JSONDecoder().decode([WorldCup].self, from: jsonData)
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return worldCups.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WorldCupTableViewCell
+
+        // pega o atual campeao
+        let worldCup = worldCups[indexPath.row]
+        
+        // Configure the cell...
+        
+        // Configure the cell...
+//        cell.textLabel?.text = "Copa \(worldCup.year) - \(worldCup.country)"
+//        cell.detailTextLabel?.text = "\(worldCup.winner) \(worldCup.winnerScore) x \(worldCup.viceScore) \(worldCup.vice)"
+//        cell.imageView?.image = UIImage(named: worldCup.winner)
+        cell.prepare(with: worldCup)
+        
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
